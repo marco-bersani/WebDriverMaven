0fc4dab7fbe33f81d5c3ecdec61d4387905a2e7d
Marco Bersani <marco.bersani@spot.it>
2020-04-28 15:27:01 +0200

    Initial commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,38 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+
+	}
+
+	@Test
+	public void doLogin() {
+
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("marco.bersani@spot.it");
+		driver.findElement(By.id("identifierNext")).click();
+		// driver.findElement(By.id("password")).sendKeys("sdsd");
+
+	}
+
+	@AfterSuite
+	public void tearDown() {
+
+		driver.quit();
+	}
+
+}
